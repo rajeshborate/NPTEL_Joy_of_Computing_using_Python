b01f43fd789432d8f1ce05a30dbad95d66453e97
RAJESH D BORATE <rajeshborate08@gmail.com>
2019-02-28 08:46:47 +0530

    Add files via upload

---
+++ Programming_Assignment_02_large.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 28 08:36:05 2019
+
+@author: Rajesh D Borate
+"""
+
+a,b=input().split(" ")
+a=int(a)
+b=int(b)
+
+if(a>b):
+    print(a)
+else:
+    print(b)
